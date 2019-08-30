278e9e9143aba0ab25db76affe32f0ba07f327a1
comptonben <comptonben504@gmail.com>
2019-08-30 02:32:37 -0500

    adding equals

---
+++ equals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object o = ".NET Interview questions";
+            object o1 = new string(".NET Interview questions".ToCharArray());
+
+            // *string data type always does content comparisons*
+            Console.WriteLine(o == o1);         // compares references not contents
+            Console.WriteLine(o.Equals(o1));    // compares contents not references
+        }
+    }
+}
